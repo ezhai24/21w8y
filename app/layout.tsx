f9be3912ab67672deb3bdacfd4c056970ff23209
Emily Zhai <ezhai24@gmail.com>
2023-10-14 08:18:00 -0400

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
 import { Dela_Gothic_One } from "next/font/google";
+import type { Metadata } from "next";
 import "./globals.css";
 
 const delaGothicOne = Dela_Gothic_One({
   weight: "400",
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  title: "Emily Zhai | 21W8Y Portfolio",
+  description:
+    "An unofficial portfolio for the Unsplash photographer Mateusz Delegacz",
+};
 
 export default function RootLayout({
   children,
